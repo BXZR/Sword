ba731b37de41511ec2ee4fac8578c7b33393b4f3
BXZR <405181821@qq.com>
2018-04-09 21:54:17 +0800

    装备配置界面UI框架
    装备配置界面的UI框架制作
    以后需要根据这个框架来加肉和资源

--- Assets/Code/UI/packagePanel/thePackagePanelShow.cs
+++ Assets/Code/UI/packagePanel/thePackagePanelShow.cs
@@ -8,13 +8,22 @@ public class thePackagePanelShow : MonoBehaviour {
 	//背包界面的总控制单元
 	public Text theBasicShowText ;
 
-
-	
 	//因为背包的东西可能会经常换，所以暂时还真需要适时刷新
 	//等待后面的事件机制完全了或许就好了
-	void Update ()
+	void OnEnable()
 	{
-		if (systemValues.thePlayer)
-			theBasicShowText.text = "当前负重: "+ systemValues.thePlayer.weightPercent *100 +"%";
+		if (systemValues.thePlayer) 
+		{
+			theBasicShowText.text = "当前负重: " + systemValues.thePlayer.weightPercent * 100 + "%";
+			theBasicShowText.text += "\n攻击力+100";
+			theBasicShowText.text += "\n攻击力+100";
+			theBasicShowText.text += "\n攻击力+100";
+			theBasicShowText.text += "\n攻击力+100";
+			theBasicShowText.text += "\n攻击力+100";
+			theBasicShowText.text += "\n攻击力+100";
+			theBasicShowText.text += "\n攻击力+100";
+			theBasicShowText.text += "\n攻击力+100";
+		}
 	}
+
 }
