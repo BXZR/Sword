3af7499fb08233471ccbdc35b1bad0b38a33a996
BXZR <405181821@qq.com>
2018-05-04 10:24:42 +0800

    竖版的文字
    竖版的文字，虽然只是五灵系统的一个装饰，但是仍然需要好好注意

---
+++ Assets/Code/UI/ExtraControl/columnText.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class columnText : MonoBehaviour {
+
+	//Text文本编程竖版
+
+	void Start () 
+	{
+		Text theText = this.GetComponent <Text> ();
+		if (theText)
+			theText.text = systemValues.rowStringToColumn (theText.text , 35 , false);
+		Destroy (this);
+	}
+	
+
+}
