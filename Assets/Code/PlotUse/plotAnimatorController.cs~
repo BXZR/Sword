1c265967b4ae10678c786b533ae5912d3f38c3ad
BXZR <405181821@qq.com>
2018-06-01 15:35:48 +0800

    剧情架构声音和动作
    剧情架构声音和动作
    增加的内容很少，但是可扩展性和可已知性都很好。
    操控剧情系统再加额外脚本就好，希望能够保留这些特性。

---
+++ Assets/Code/PlotUse/plotAnimatorController.cs~
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class plotAnimatorController : MonoBehaviour , plotActions  {
+
+	//每一个剧本帧的音效控制单元
+	public GameObject thePlayer;
+	public string theStateName;
+	private Animator theAnimator;
+	//没办法，接口默认pulic
+	//开始的时候统一调用
+	public  void OnStart (plotItem theItem)
+	{
+		theAnimator = thePlayer.GetComponentInChildren<Animator> ();
+		if (theAnimator)
+			theAnimator.Play (theStateName);
+	}
+
+	//结束的时候统一调用
+	public  void OnEnd()
+	{
+	}
+
+	//每一帧更新的时候统一调用
+	public  void OnUpdate()
+	{
+	}
+
+}
---
+++ Assets/Code/PlotUse/plotSoundController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(AudioSource))]
+public class plotSoundController : MonoBehaviour , plotActions  {
+
+	//每一个剧本帧的音效控制单元
+	public AudioClip theClip;
+	private AudioSource theSouce;
+	void Start ()
+	{
+		theSouce = this.GetComponent <AudioSource> ();
+	}
+	
+	//没办法，接口默认pulic
+	//开始的时候统一调用
+	public  void OnStart (plotItem theItem)
+	{
+		if (theSouce)
+			theSouce.PlayOneShot (theClip);
+	}
+
+	//结束的时候统一调用
+	public  void OnEnd()
+	{
+	}
+
+	//每一帧更新的时候统一调用
+	public  void OnUpdate()
+	{
+	}
+
+}
