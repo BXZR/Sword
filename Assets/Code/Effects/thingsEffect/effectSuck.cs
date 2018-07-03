9d27ad389f48facf40f71fc10ea6541925799744
BXZR <405181821@qq.com>
2018-07-03 12:26:15 +0800

    继续强迫症吸神诀
    继续强迫症吸神诀
    （其实现在更应该扩展人物传记的buff，这个今天后期看看能不能给出来）

--- Assets/Code/Effects/thingsEffect/effectSuck.cs
+++ Assets/Code/Effects/thingsEffect/effectSuck.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class effectSuck : effectBasic {
 
 
-	int  count = 0;
-	int countMax =3;
+	int attackCount = 0;
+	int attackCountMax =3;
 	float speednow = 0f;
 	float speedMax = 0.15f;
 	float addCount = 0;
-	float addMax = 100;
+	float addMax = 80;
 
 	void Start ()
 	{
@@ -23,7 +23,7 @@ public class effectSuck : effectBasic {
 		lifeTimerAll = 90f;//每一个段时间才能够使用这个伤害
 		timerForEffect = 90f; 
 		theEffectName = "吸神诀";
-		theEffectInformation = "闪避、格挡触发时回复40%最大斗气\n攻击起手吸取身边所有目标1%最大生命值\n每第4次攻击附加以下额外效果：\n吸取目标2%最大生命值，暴击时吸收双倍\n永久吸取目标3%攻击力和护甲(上限100次)\n攻速和移速加成效果提升1%(上限15%)";
+		theEffectInformation = "闪避、格挡触发时回复40%最大斗气\n攻击起手吸取身边所有目标1%最大生命值\n每第4次攻击附加以下额外效果：\n吸取目标2%最大生命值，暴击时吸收双倍\n永久吸取目标3%攻击力和护甲(上限80次)\n攻速和移速加成效果提升1%(上限15%)";
 		theEffedctExtraInformation = "特性：吸神之意，受到攻击时将伤害的7%转化为护盾\n特性：吸海无涯，升级时额外增加2%护盾上限";
 		makeStart ();
 		Destroy (this,lifeTimerAll);
@@ -41,15 +41,20 @@ public class effectSuck : effectBasic {
 		return "等级奖励\n获得吸神诀时等级超过2级，额外永久获得6%斗气偷取";
 	}
 
+	public override string getOnTimeFlashInformation ()
+	{
+		return this.theEffectName +"\n"+addCount+"|"+(speednow*100).ToString("f0")+"%";
+	}
+
 
 	public override void OnAttack (PlayerBasic aim, float TrueDamage)
 	{
-		count++;
-		if (count == countMax)
+		attackCount++;
+		if (attackCount == attackCountMax)
 		{
-			count = 0;
+			attackCount = 0;
 
-			if (addCount < addMax)
+			if ((addCount +1) <= addMax)
 			{
 				addCount++;
 				thePlayer.ActerWuliDamage += aim.ActerWuliDamage * 0.03f;
@@ -58,7 +63,7 @@ public class effectSuck : effectBasic {
 				thePlayer.CActerWuliShield += aim.ActerWuliShield * 0.03f;
 			}
 
-			if (speednow < speedMax)
+			if ((speednow + 0.01f) < speedMax)
 			{
 				thePlayer.ActerSpeedOverPervnet += 0.01f;
 				speednow +=  0.01f;
